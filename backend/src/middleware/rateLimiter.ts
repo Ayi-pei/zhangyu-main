34c9d6dd46edc757fef0339f90e7e06a8d7e8e2b
阿义 <ayi198701@gmail.com>
2025-03-01 16:20:05 +0800

    提交代码更新

--- backend/src/middleware/rateLimiter.ts
+++
@@ -1,7 +0,0 @@
-import rateLimit from 'express-rate-limit';
-
-export const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15分钟
-  max: 100, // 每个IP在windowMs时间内最多100个请求
-  message: '请求过于频繁，请稍后再试'
-}); 
